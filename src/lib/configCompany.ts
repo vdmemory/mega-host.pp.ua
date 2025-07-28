03da49ad37a472064a025bff7bf37a4e9d41730e
Vadim Ivchenko <47832273+vdmemory@users.noreply.github.com>
2025-07-28 14:39:31 +0300

    update social links

--- src/lib/configCompany.ts
+++ src/lib/configCompany.ts
@@ -4,15 +4,15 @@ export const configCompany = {
     infoEng: 'LLC "BLAIVIZTORG" EDRPOU 45471414',
     email: 'deinegam1@ukr.net',
     phone: '+380630327589',
-    address: 'Україна,69083  місто Київ, вул.  Юри Гната, буд.3',
+    address: 'Україна, 69083  місто Київ, вул. Юри Гната, буд.3',
     addressEng: 'Ukraine, 69083 Kyiv, Yurii Hnata St, 3',
     formEmail: 'example@gmail.com',
 
-    olx: 'https://www.olx.ua/uk/list/user/2hseJj',
-    prom: 'https://prom.ua/c4122303-tov-smart-horizons.html',
-    facebook: 'https://www.facebook.com/profile.php?id=61578364689074',
-    instagram: 'https://www.instagram.com/silver.development/',
-    linkedin: 'https://www.linkedin.com/in/max-capran-b0a5a3376/',
+    olx: 'https://www.olx.ua/',
+    prom: 'https://prom.ua/ua/c4123874-tov-blejviztorg.html',
+    facebook: 'https://www.facebook.com/profile.php?id=61579033851195',
+    instagram: 'https://www.instagram.com/meg.ahost/',
+    linkedin: 'https://www.linkedin.com/',
 
-    whatsapp: 'https://www.whatsapp.com/silverDevelopment', // ???
+    whatsapp: 'https://www.whatsapp.com/', // ???
 };
